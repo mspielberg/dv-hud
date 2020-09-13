ed303e38a7fc4b8b57ae63d111baba22f94bf897
mspielberg <9729801+mspielberg@users.noreply.github.com>
2020-09-13 15:46:59 +0000

    Use plus and minus signs for grade display

--- GeneralProviders.cs
+++ GeneralProviders.cs
@@ -6,6 +6,11 @@ namespace DvMod.HeadsUpDisplay
 {
     static class GeneralProviders
     {
+        // U+2007 FIGURE SPACE
+        // U+002B PLUS SIGN
+        // U+2212 MINUS SIGN
+        private const string GradeFormat = "\u002b0.0' %';\u22120.0' %'";
+
         public static void Register()
         {
             Registry.Register(RegistryKeys.AllCars, new QueryDataProvider(
@@ -21,7 +26,7 @@ namespace DvMod.HeadsUpDisplay
                     inclination = inclination > 180 ? 360f - inclination : -inclination;
                     return Mathf.Tan(inclination * Mathf.PI / 180) * 100;
                 },
-                f => $"{f.ToString("F1")} %"));
+                f => f.ToString(GradeFormat)));
             Registry.Register(RegistryKeys.AllCars, new QueryDataProvider(
                 "Brake pipe",
                 () => Main.settings.ShowBrakePipe,
