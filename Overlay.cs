ec02478ceaa773eab4cbd272b354c7f7ea1c6ca6
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-08-08 18:07:29 -0700

    Split event list rendering into separate class

--- Overlay.cs
+++ Overlay.cs
@@ -50,7 +50,7 @@ namespace DvMod.HeadsUpDisplay
 
     public class Overlay : MonoBehaviour
     {
-        private const int ColumnSpacing = 10;
+        public const int ColumnSpacing = 10;
 
         public static Overlay? instance;
 
@@ -107,7 +107,7 @@ namespace DvMod.HeadsUpDisplay
             if (Main.settings.showDrivingInfo)
                 DrawCurrentCarInfo();
             if (Main.settings.trackInfoSettings.enabled)
-                DrawUpcomingEvents();
+                EventList.DrawUpcomingEvents();
 
             GUILayout.EndHorizontal();
 
@@ -577,116 +577,6 @@ namespace DvMod.HeadsUpDisplay
                 DrawColumn(groups, "Cyl", g => g.maxBrakeCylinderPressure.ToString("F2"));
             }
             DrawColumn(groups, "Force", g => g.maxBrakeFactor.ToString("P0"), Styles.rightAlign);
-        }
-
-        private string GetJunctionEventDescription(JunctionEvent e)
-        {
-            var description = TrackFollower.DescribeJunctionBranches(e.junction);
-            var car = GetCarOnJunction(e.junction);
-            var carText = (car is Car && car != PlayerManager.Car.logicCar)
-                ? $" <color=orange>({car.ID})</color>" : "";
-            return description + carText;
-        }
-
-        private string GetSpeedLimitEventDescription(SpeedLimitEvent e)
-        {
-            var currentSpeed = Mathf.Abs(PlayerManager.Car.GetForwardSpeed() * 3.6f);
-            var color = "white";
-            if (currentSpeed > e.limit + 5f)
-                color = e.span < 500f ? "red" : e.span < 1000f ? "orange" : "yellow";
-            else if (currentSpeed < e.limit - 10f)
-                color = "lime";
-            return $"<color={color}>{e.limit} km/h</color>";
-        }
-
-        private void DrawUpcomingEvents()
-        {
-            if (!PlayerManager.Car)
-                return;
-
-            var trackInfoSettings = Main.settings.trackInfoSettings;
-
-            var bogie = PlayerManager.Car.Bogies[1];
-            var track = bogie.track;
-            if (track == null)
-                return;
-            var startSpan = bogie.traveller.Span;
-            var locoDirection = PlayerManager.LastLoco == null || PlayerManager.LastLoco.GetComponent<LocoControllerBase>()?.reverser >= 0f;
-            var direction = !locoDirection ^ (bogie.trackDirection > 0);
-            var currentGrade = TrackIndexer.Grade(bogie.point1) * (direction ? 1 : -1);
-
-            var events = TrackFollower.FollowTrack(
-                track,
-                startSpan,
-                direction ? trackInfoSettings.maxEventSpan : -trackInfoSettings.maxEventSpan);
-
-            var eventDescriptions = events
-                .ExceptUnnamedTracks()
-                .ResolveJunctionSpeedLimits()
-                .FilterRedundantSpeedLimits()
-                .FilterGradeEvents(currentGrade)
-                .Take(trackInfoSettings.maxEventCount)
-                .TakeWhile(ev => ev.span < trackInfoSettings.maxEventSpan)
-                .Select(ev => ev switch
-                    {
-                        TrackChangeEvent e => (e.span, e.ID.ToString()),
-                        JunctionEvent e => (e.span, GetJunctionEventDescription(e)),
-                        DualSpeedLimitEvent e => (e.span, $"{e.limit} / {e.rightLimit} km/h"),
-                        SpeedLimitEvent e => (e.span, GetSpeedLimitEventDescription(e)),
-                        GradeEvent e => (e.span, $"{e.grade:F1} %"),
-                        _ => (0.0, $"Unknown event: {ev}"),
-                    })
-                .ToList();
-
-            GUILayout.BeginHorizontal("box", GUILayout.ExpandHeight(true));
-
-            GUILayout.BeginVertical(GUILayout.MaxWidth(50));
-            foreach ((double span, string desc) in eventDescriptions)
-                GUILayout.Label($"{Math.Round(span / 10) * 10:F0} m", Styles.rightAlign);
-            for (int i = eventDescriptions.Count; i < trackInfoSettings.maxEventCount; i++)
-                GUILayout.Label(" ");
-            GUILayout.EndVertical();
-
-            GUILayout.Space(ColumnSpacing);
-
-            GUILayout.BeginVertical();
-            foreach ((double span, string desc) in eventDescriptions)
-                GUILayout.Label(desc, Styles.richText);
-            for (int i = eventDescriptions.Count; i < trackInfoSettings.maxEventCount; i++)
-                GUILayout.Label(" ");
-            GUILayout.EndVertical();
-
-            GUILayout.EndHorizontal();
-        }
-
-        public static Car? GetCarOnJunction(Junction junction)
-        {
-            static double DistanceToBranch(Junction.Branch branch, TrainCar car)
-            {
-                return car.Bogies
-                    .Where(bogie => bogie.track == branch.track)
-                    .Min(bogie => branch.first ? bogie.traveller.Span : branch.track.logicTrack.length - bogie.traveller.Span);
-            }
-
-            static (double, Car)? ClosestCar(Junction.Branch branch)
-            {
-                var logicTrack = branch.track.logicTrack;
-                var logicCarToTrainCar = SingletonBehaviour<IdGenerator>.Instance.logicCarToTrainCar;
-                var allCars = logicTrack.GetCarsFullyOnTrack().Concat(logicTrack.GetCarsPartiallyOnTrack());
-                if (!allCars.Any())
-                    return null;
-                var byDistance = allCars.ToDictionary(car => DistanceToBranch(branch, logicCarToTrainCar[car]));
-                var minDistance = byDistance.Keys.Min();
-                return (minDistance, byDistance[minDistance]);
-            }
-
-            const double SpanTolerance = 7.0;
-            var branches = junction.outBranches.Append(junction.inBranch);
-            var closest = branches.Select(ClosestCar).OfType<(double, Car)>().OrderBy(p => p.Item1).FirstOrDefault();
-
-            if (closest.Item1 < SpanTolerance && closest.Item2 != null)
-                return closest.Item2;
-            return null;
         }
     }
 }
